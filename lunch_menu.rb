5833ea7ed855e21da3c338d74656e2a0af81c0ac
pcbennett108 <pcbennett42@gmail.com>
2023-05-25 10:05:46 -0600

    Add block for purchase

--- lunch_menu.rb
+++ lunch_menu.rb
@@ -5,8 +5,20 @@ def noon
   if lunch.length > 5
     puts "munchies woot!"
   else
-    puts "Oh noes!"
+    puts "Oh noes! I'll buy a PearPad instead."
   end
 end
 
 noon
+
+def purchase
+  account_bal = 1256
+  price_of_PearPad = 1299
+  if account_bal > price_of_PearPad
+    puts "WoooHoo!!"
+  else
+    puts "Noooo!"
+  end
+end
+
+purchase
